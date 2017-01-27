5b6c485d9b1bb1d6ca23c792f585c1cb4cd47aaa
Till Kahlbrock <till@jimdo.com>
2017-01-27 11:37:06 +0100

    allows to deactivate tasks and check for their status

--- src/main/java/com/github/tillkahlbrock/todo/Task.java
+++ src/main/java/com/github/tillkahlbrock/todo/Task.java
@@ -3,12 +3,23 @@ package com.github.tillkahlbrock.todo;
 class Task {
 
     private String title;
+    private boolean active = true;
 
-    Task(String title) {
+    Task(String title)
+    {
         this.title = title;
     }
 
     String title() {
         return title;
+    }
+
+    boolean isActive() {
+        return this.active;
+    }
+
+    void deactivate()
+    {
+        this.active = false;
     }
 }
--- src/test/java/com/github/tillkahlbrock/todo/TodoListTest.java
+++ src/test/java/com/github/tillkahlbrock/todo/TodoListTest.java
@@ -3,15 +3,45 @@ package com.github.tillkahlbrock.todo;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TodoListTest
 {
     @Test
     void itShouldAddATaskWithTheGivenTitle() {
-        TodoList todoList = this.subject();
-        todoList.addTask("Do something!");
+        TodoList todoList = aTodoListWithOneTask("Do something!");
         assertEquals(1, todoList.allTasks().size());
         assertEquals("Do something!", todoList.allTasks().get(0).title());
+    }
+
+    @Test
+    void itShouldActivateATaskWhenAdded()
+    {
+        TodoList todoList = aTodoListWithOneTask();
+        Task task = todoList.allTasks().get(0);
+        assertTrue(task.isActive());
+    }
+
+    @Test
+    void itShouldDeactivateASpecificTask()
+    {
+        TodoList todoList = this.aTodoListWithOneTask();
+        Task task = todoList.allTasks().get(0);
+        task.deactivate();
+        assertFalse(task.isActive());
+    }
+
+    private TodoList aTodoListWithOneTask()
+    {
+        return this.aTodoListWithOneTask("Some title");
+    }
+
+    private TodoList aTodoListWithOneTask(String title)
+    {
+        TodoList todoList = this.subject();
+        todoList.addTask(title);
+        return todoList;
     }
 
     private TodoList subject() {
