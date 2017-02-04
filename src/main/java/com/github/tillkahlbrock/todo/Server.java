f40d37fcfa73cc12f9e26439e9427d117db496d6
Till Kahlbrock <till@jimdo.com>
2017-02-04 16:10:42 +0100

    exposes add task action over http api

--- src/main/java/com/github/tillkahlbrock/todo/Server.java
+++ src/main/java/com/github/tillkahlbrock/todo/Server.java
@@ -6,9 +6,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import static spark.Spark.get;
-import static spark.Spark.port;
+import static spark.Spark.*;
 
 @Configuration
 @ComponentScan
@@ -23,6 +21,7 @@ public class Server
         TodoList todoList = context.getBean(TodoList.class);
         logger.trace("Starting to serve...");
         get("/tasks", (req, res) -> todoList.toJson());
+        post("/task", (req, res) -> todoList.addTask(req.queryParams("title")));
         logger.trace("Done!");
     }
 }
